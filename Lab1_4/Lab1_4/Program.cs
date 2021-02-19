7c96b7fd01a82ff881a7c0fe88a25eae6a92a179
kirasir1 <zebra46@gmail.com>
2021-02-19 11:06:16 +0300

    Сделано задание 4

---
+++ Lab1_4/Lab1_4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Lab1_4
+{
+    class Worker
+    {
+        public string Name { get; set; }
+        public int ID { get; set; }
+
+    }
+
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using (FileStream fs = new FileStream("person.json", FileMode.OpenOrCreate))
+            {
+                Worker tom = new Worker() { Name = "R6853", ID = 5060396 };
+                await JsonSerializer.SerializeAsync<Worker>(fs, tom);
+                Console.WriteLine("Данные сохранены в файл");
+            }
+            
+            using (FileStream fs = new FileStream("person.json", FileMode.OpenOrCreate))
+            {
+                Worker restoredPerson = await JsonSerializer.DeserializeAsync<Worker>(fs);
+                Console.WriteLine($"Name: {restoredPerson.Name}  Age: {restoredPerson.ID}");
+            }
+
+        }
+    }
+
+}
