61866125c0358c86b63eb33dd85f03b6ae49ef2b
kerbal <zebra46@gmail.com>
2021-02-22 00:45:15 +0300

    Оптимизация

--- Labs1/Lab1_3/Lab1_3/Program.cs
+++ Labs1/Lab1_3/Lab1_3/Program.cs
@@ -6,15 +6,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        string[] delete = {".//archive", ".//text_dezip", "text.zip"};
-        FileInfo check = new FileInfo(delete[2]);
+        string[] namelist = {".//archive", ".//text_dezip", "text.zip"};
+        FileInfo check = new FileInfo(namelist[2]);
         if (check.Exists)
         {
-            File.Delete(delete[2]);
+            File.Delete(namelist[2]);
             try
             {
-                Directory.Delete(delete[0], true);
-                Directory.Delete(delete[1], true);
+                Directory.Delete(namelist[0], true);
+                Directory.Delete(namelist[1], true);
             }
             catch
             {}
@@ -34,22 +34,19 @@ class Program
         }
         Console.WriteLine("Введите строку для записи в файл:");
         string text = Console.ReadLine();
-        string path1 = ".//archive";
-        DirectoryInfo dirInfo1 = new DirectoryInfo(path1);
+        DirectoryInfo dirInfo1 = new DirectoryInfo(namelist[0]);
         dirInfo1.Create();
-        using (FileStream fstream = new FileStream($"{path1}\\note.txt", FileMode.OpenOrCreate))
+        using (FileStream fstream = new FileStream($"{namelist[0]}\\note.txt", FileMode.OpenOrCreate))
         {
             byte[] array = System.Text.Encoding.Default.GetBytes(text);
             fstream.Write(array, 0, array.Length);
             Console.WriteLine("Текст записан в файл");
         }
         Console.WriteLine();
-        string zipfile = "text.zip";
-        string dezipfolder = ".//text_dezip";
-        ZipFile.CreateFromDirectory(path1, zipfile);
-        Console.WriteLine($"Папка {path1} архивирована в файл {zipfile}");
+        ZipFile.CreateFromDirectory(namelist[0], namelist[2]);
+        Console.WriteLine($"Папка {namelist[0]} архивирована в файл {namelist[2]}");
         Console.WriteLine();
-        FileInfo fileInf = new FileInfo("text.zip");
+        FileInfo fileInf = new FileInfo(namelist[2]);
         if (fileInf.Exists)
         {
             Console.WriteLine("Имя файла: {0}", fileInf.Name);
@@ -59,10 +56,10 @@ class Program
         }
         Console.WriteLine("Нажмите Enter, чтобы продолжить...");
         Console.ReadLine();
-        DirectoryInfo dirInfo2 = new DirectoryInfo(dezipfolder);
+        DirectoryInfo dirInfo2 = new DirectoryInfo(namelist[1]);
         dirInfo2.Create();
-        ZipFile.ExtractToDirectory(zipfile, dezipfolder);
-        Console.WriteLine($"Файл {zipfile} распакован в папку {dezipfolder}");
+        ZipFile.ExtractToDirectory(namelist[2], namelist[1]);
+        Console.WriteLine($"Файл {namelist[2]} распакован в папку {namelist[1]}");
         FileInfo fileInf2 = new FileInfo(".//text_dezip//note.txt");
         if (fileInf.Exists)
         {
