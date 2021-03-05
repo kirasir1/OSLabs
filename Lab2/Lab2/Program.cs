9d885613d9d37055d906920b9fac8b220e2f6803
kirasir1 <zebra46@gmail.com>
2021-03-05 10:31:08 +0300

    Лабораторная работа №2

---
+++ Lab2/Lab2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Security.Cryptography;
+using System.Text;
+namespace Lab2
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Thread brutethread1 = new Thread(new ThreadStart(Brute));
+    }
+    public static void Brute()
+    {
+      string path = "hashes.txt";
+      string[] line = File.ReadAllLines(path);
+
+
+    }
+  }
+}
