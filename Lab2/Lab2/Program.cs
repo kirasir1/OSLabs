4210910c776313e91ec0cfa8799d6dbcf230c9ec
kerbal <zebra46@gmail.com>
2021-03-18 22:15:48 +0300

    ы

--- Lab2/Lab2/Program.cs
+++ Lab2/Lab2/Program.cs
@@ -3,9 +3,91 @@ using System.IO;
 using System.Threading;
 using System.Security.Cryptography;
 using System.Text;
+using System.Linq;
 namespace Lab2
 {
-  class Program
+    class Program
+{
+    #region Private variables
+    
+    private static string password = "apple";
+    private static string result;
+ 
+    private static bool isMatched = false;
+    
+    private static int charactersToTestLength = 0;
+    private static long computedKeys = 0;
+    
+    private static char[] charactersToTest =
+    {
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+        'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+        'u', 'v', 'w', 'x', 'y', 'z'
+    };
+ 
+    #endregion
+ 
+    static void Main(string[] args)
+    {
+        var timeStarted = DateTime.Now;
+        Console.WriteLine("Start BruteForce - {0}", timeStarted.ToString());
+        charactersToTestLength = charactersToTest.Length;
+        var estimatedPasswordLength = 0;
+ 
+        while (!isMatched)
+        {
+          estimatedPasswordLength++;
+            startBruteForce(estimatedPasswordLength);
+        }
+ 
+        Console.WriteLine("Password matched. - {0}", DateTime.Now.ToString());
+        Console.WriteLine("Time passed: {0}s", DateTime.Now.Subtract(timeStarted).TotalSeconds);
+        Console.WriteLine("Resolved password: {0}", result);
+        Console.WriteLine("Computed keys: {0}", computedKeys);
+ 
+        Console.ReadLine();
+    }
+    private static void startBruteForce(int keyLength)
+    {
+        var keyChars = createCharArray(keyLength, charactersToTest[0]);
+        var indexOfLastChar = keyLength - 1;
+        createNewKey(0, keyChars, keyLength, indexOfLastChar);
+    }
+  
+    private static char[] createCharArray(int length, char defaultChar)
+    {
+        return (from c in new char[length] select defaultChar).ToArray();
+    }
+    
+    private static void createNewKey(int currentCharPosition, char[] keyChars, int keyLength, int indexOfLastChar)
+    {
+        var nextCharPosition = currentCharPosition + 1;
+        for (int i = 0; i < charactersToTestLength; i++)
+        {
+          keyChars[currentCharPosition] = charactersToTest[i];
+          
+            if (currentCharPosition < indexOfLastChar)
+            {
+                createNewKey(nextCharPosition, keyChars, keyLength, indexOfLastChar);
+            }
+            else
+            {
+              computedKeys++;
+              if ((new String(keyChars)) == password)
+                {
+                    if (!isMatched)
+                    {
+                        isMatched = true;
+                        result = new String(keyChars);
+                    }
+                    return;
+                }
+            }
+        }
+    }
+}
+}
+  /*class Program
   {
     static void Main(string[] args)
     {
@@ -89,5 +171,4 @@ namespace Lab2
     }
 
 
-  }
-}
+  }*/
