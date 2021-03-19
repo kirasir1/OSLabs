89e2bb2e9307929474aa199c99a41002fb15c995
Шарапов Кирилл <kirasir2001@hotmail.com>
2021-03-19 10:46:27 +0300

    л/р номер 2

--- Lab2/Lab2/Program.cs
+++ Lab2/Lab2/Program.cs
@@ -7,51 +7,52 @@ using System.Linq;
 namespace Lab2
 {
     class Program
-{
-    #region Private variables
-    
-    private static string password = "apple";
+{ 
+    private static string[] passwords = {
+      "1115dd800feaacefdf481f1f9070374a2a81e27880f187396db67958b207cbad", 
+      "3a7bd3e2360a3d29eea436fcfb7e44c735d117c42d1c1835420b6b9942dd4f1b", 
+      "74e1bb62f8dabb8125a58852b63bdf6eaef667cb56ac7f7cdba6d7305c50a22f"
+    };
     private static string result;
- 
-    private static bool isMatched = false;
-    
     private static int charactersToTestLength = 0;
     private static long computedKeys = 0;
-    
+    static Mutex locker = new Mutex();
     private static char[] charactersToTest =
     {
         'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
         'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
         'u', 'v', 'w', 'x', 'y', 'z'
     };
- 
-    #endregion
- 
     static void Main(string[] args)
     {
-        var timeStarted = DateTime.Now;
-        Console.WriteLine("Start BruteForce - {0}", timeStarted.ToString());
-        charactersToTestLength = charactersToTest.Length;
-        var estimatedPasswordLength = 0;
- 
-        while (!isMatched)
-        {
-          estimatedPasswordLength++;
-            startBruteForce(estimatedPasswordLength);
-        }
- 
-        Console.WriteLine("Password matched. - {0}", DateTime.Now.ToString());
-        Console.WriteLine("Time passed: {0}s", DateTime.Now.Subtract(timeStarted).TotalSeconds);
-        Console.WriteLine("Resolved password: {0}", result);
-        Console.WriteLine("Computed keys: {0}", computedKeys);
- 
-        Console.ReadLine();
+      for (int i=0; i<3; i++)
+      {
+        Thread ytfug = new Thread(new ParameterizedThreadStart(StartProgram));
+        ytfug.Start(passwords[i]);
+      }
+      //StartProgram();
     }
-    private static void startBruteForce(int keyLength)
+    private static void StartProgram(object pasnumber)
+    {
+      locker.WaitOne();
+        var timeStarted = DateTime.Now;
+        Console.WriteLine("Время начала подбора - {0}", timeStarted.ToString());
+        charactersToTestLength = charactersToTest.Length;
+        var estimatedPasswordLength = 5;
+        string pswd = pasnumber.ToString();
+        startBruteForce(estimatedPasswordLength, pswd);
+        Console.WriteLine("Паролей подобрано - {0}", DateTime.Now.ToString());
+        Console.WriteLine("Прошло времени: {0} сек", DateTime.Now.Subtract(timeStarted).TotalSeconds);
+        Console.WriteLine("Полученный пароль: {0}", result);
+        Console.WriteLine("Вычислено паролей: {0}", computedKeys);
+      locker.ReleaseMutex();
+    }
+    private static void startBruteForce(int keyLength, string pswd)
     {
         var keyChars = createCharArray(keyLength, charactersToTest[0]);
         var indexOfLastChar = keyLength - 1;
-        createNewKey(0, keyChars, keyLength, indexOfLastChar);
+      Console.WriteLine(pswd);
+      createNewKey(0, keyChars, keyLength, indexOfLastChar, pswd);
     }
   
     private static char[] createCharArray(int length, char defaultChar)
@@ -59,7 +60,7 @@ namespace Lab2
         return (from c in new char[length] select defaultChar).ToArray();
     }
     
-    private static void createNewKey(int currentCharPosition, char[] keyChars, int keyLength, int indexOfLastChar)
+    private static void createNewKey(int currentCharPosition, char[] keyChars, int keyLength, int indexOfLastChar, string pswd)
     {
         var nextCharPosition = currentCharPosition + 1;
         for (int i = 0; i < charactersToTestLength; i++)
@@ -68,107 +69,31 @@ namespace Lab2
           
             if (currentCharPosition < indexOfLastChar)
             {
-                createNewKey(nextCharPosition, keyChars, keyLength, indexOfLastChar);
+                createNewKey(nextCharPosition, keyChars, keyLength, indexOfLastChar, pswd);
             }
             else
             {
               computedKeys++;
-              if ((new String(keyChars)) == password)
+              if (GetHashString(new String(keyChars)) == pswd)
                 {
-                    if (!isMatched)
-                    {
-                        isMatched = true;
-                        result = new String(keyChars);
-                    }
+                    result = new String(keyChars);
                     return;
                 }
             }
         }
     }
-}
-}
-  /*class Program
-  {
-    static void Main(string[] args)
+    public static byte[] GetHash(string inputString)
     {
-      Brute();
+      using (HashAlgorithm algorithm = SHA256.Create())
+        return algorithm.ComputeHash(Encoding.UTF8.GetBytes(inputString));
     }
-    public static void Brute()
+    public static string GetHashString(string inputString)
     {
-      SHA256 sha256Hash = SHA256.Create();
-      string path = "hashes.txt";
-      var asciibytes = new byte []{ 97, 97, 97, 97, 97};
-      string[] line = File.ReadAllLines(path);
-      var text = Encoding.ASCII.GetString(asciibytes);
-      Console.WriteLine(text);
-      string hash = GetHash(sha256Hash, text);
-        for (int i=0; i<25; i++)
-        {
-          for (int j = 0; j < 25; j++)
-          {
-            for (int k = 0; k < 25; k++)
-            {
-              for (int l= 0; l < 25; l++)
-              {
-                for (int g = 0; g < 25; g++)
-                {
-                  asciibytes[4]+=1;
-                  text = Encoding.ASCII.GetString(asciibytes);
-                  Console.WriteLine(text);
-                  hash = GetHash(sha256Hash, text);
-                if (hash == line[0] || hash == line[1] || hash == line[2]) return;
-                }
-                if (asciibytes[4] == 122)
-                {
-                  asciibytes[4] = 97;
-                }
-                asciibytes[3]+=1;
-                text = Encoding.ASCII.GetString(asciibytes);
-                Console.WriteLine(text);
-                hash = GetHash(sha256Hash, text);
-              if (hash == line[0] || hash == line[1] || hash == line[2]) return;
-            }
-              if (asciibytes[3] == 122)
-              {
-                asciibytes[3] = 97;
-              }
-              asciibytes[2]+=1;
-              text = Encoding.ASCII.GetString(asciibytes);
-              Console.WriteLine(text);
-              hash = GetHash(sha256Hash, text);
-            if (hash == line[0] || hash == line[1] || hash == line[2]) return;
-          }
-            if (asciibytes[2] == 122)
-            {
-              asciibytes[2] = 97;
-            }
-            asciibytes[1]+=1;
-            text = Encoding.ASCII.GetString(asciibytes);
-            Console.WriteLine(text);
-            hash = GetHash(sha256Hash, text);
-          if (hash == line[0] || hash == line[1] || hash == line[2]) return;
-        }
-          if (asciibytes[1] == 122)
-          {
-            asciibytes[1] = 97;
-          }
-          asciibytes[0]+=1;
-          text = Encoding.ASCII.GetString(asciibytes);
-          Console.WriteLine(text);
-          hash = GetHash(sha256Hash, text);
-        if (hash == line[0] || hash == line[1] || hash == line[2]) return;
-      }
-    }
-    private static string GetHash(HashAlgorithm hashAlgorithm, string input)
-    {
-      byte[] data = hashAlgorithm.ComputeHash(Encoding.UTF8.GetBytes(input));
-      var sBuilder = new StringBuilder();
-      for (int i = 0; i < data.Length; i++)
-      {
-        sBuilder.Append(data[i].ToString("x2"));
-      }
-      return sBuilder.ToString();
+      StringBuilder sb = new StringBuilder();
+      foreach (byte b in GetHash(inputString))
+        sb.Append(b.ToString("X2"));
+      return sb.ToString().ToLower();
     }
 
-
-  }*/
+  }
+}
